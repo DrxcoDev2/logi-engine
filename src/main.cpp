1fb2e90111fe2b8b27cd7020bf7b5b1018aa0815
DrxcoDev2 <drxcofn2023@gmail.com>
2025-06-01 17:45:28 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Fallo al inicializar GLFW\n";
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Editor 3D - OpenGL", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Fallo al crear la ventana\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Fallo al inicializar GLEW\n";
+        return -1;
+    }
+
+    std::cout << "OpenGL " << glGetString(GL_VERSION) << std::endl;
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Aqu� va el renderizado
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
