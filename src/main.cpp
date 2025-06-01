97b4085c803902e686607dd6c5c1da99047ee1c9
DrxcoDev2 <drxcofn2023@gmail.com>
2025-06-01 18:06:41 +0200

    shaders add: Gui.pde

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,34 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+// ImGui
+#include "../imgui/imgui.h"
+#include "../imgui/backends/imgui_impl_glfw.h"
+#include "../imgui/backends/imgui_impl_opengl3.h"
+
+void renderSidebar() {
+    ImGuiIO& io = ImGui::GetIO();
+    ImGui::SetNextWindowPos(ImVec2(0, 0));
+    ImGui::SetNextWindowSize(ImVec2(250, io.DisplaySize.y));
+
+    ImGui::Begin("Panel Lateral", nullptr,
+        ImGuiWindowFlags_NoTitleBar |
+        ImGuiWindowFlags_NoResize |
+        ImGuiWindowFlags_NoMove |
+        ImGuiWindowFlags_NoCollapse);
+
+    ImGui::Text("Herramientas:");
+    ImGui::Button("Seleccionar");
+    ImGui::Button("Mover");
+    ImGui::Button("Rotar");
+    ImGui::Button("Escalar");
+
+    ImGui::End();
+}
+
 int main() {
     if (!glfwInit()) {
-        std::cerr << "Fallo al inicializar GLFW\n";
+        std::cerr << "GLFW init failed\n";
         return -1;
     }
 
@@ -12,31 +37,51 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "Editor 3D - OpenGL", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Editor3D", NULL, NULL);
     if (!window) {
-        std::cerr << "Fallo al crear la ventana\n";
+        std::cerr << "Window creation failed\n";
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
+    glewInit();
 
-    if (glewInit() != GLEW_OK) {
-        std::cerr << "Fallo al inicializar GLEW\n";
-        return -1;
-    }
+    // Setup Dear ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
 
-    std::cout << "OpenGL " << glGetString(GL_VERSION) << std::endl;
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
 
     while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        // Start ImGui Frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        renderSidebar();
+
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Aqu� va el renderizado
-
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(window);
-        glfwPollEvents();
     }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     glfwDestroyWindow(window);
     glfwTerminate();
